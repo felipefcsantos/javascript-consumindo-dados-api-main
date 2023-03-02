9304b27f30850604e5abf4a31882c6b1c42caf3a
Felipe Santos <felipesantos_fcs98@hotmail.com>
2023-03-02 15:59:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json()
+        if (consultaCEPConvertida.erro) {
+            console.log('CEP não existente!');
+        }
+        var cidade = document.getElementById('cidade')
+        var logradouro = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
+        var bairro = document.getElementById('bairro')
+
+        cidade.value = consultaCEPConvertida.localidade
+        logradouro.value = consultaCEPConvertida.logradouro
+        estado.value = consultaCEPConvertida.uf
+        bairro.value = consultaCEPConvertida.bairro
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP Inválido. Tente Novamente!</p>`
+        console.log('Deu ruim parceiro');
+    }
+}
+ var cep = document.getElementById('cep')
+ cep.addEventListener("focusout", () => buscaEndereco(cep.value))
